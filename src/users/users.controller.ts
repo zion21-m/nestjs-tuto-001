32870a277a824e05593da5550110618b492bce1a
Merdie Mulaba <merdiemlb@gmail.com>
2023-12-05 15:56:53 +0100

    nestjs tutorial project

---
+++ src/users/users.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  /*
+    
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+     */
+  @Get() // GET /users
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+  @Get('interns') // GET /users/interns
+  findAllInters() {
+    return [];
+  }
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
